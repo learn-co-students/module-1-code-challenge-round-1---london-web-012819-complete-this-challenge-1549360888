c609ba0877545dafdb447a94baa1ed9da8f9fb59
Kian Edalat <akedalat@gmail.com>
2019-02-05 10:31:54 +0000

    skeleton

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,25 @@
 class Customer
   attr_accessor :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+@@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+def self.find_by_name(name)
+  # @@all.find do |customer|
+  # end
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,15 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Customer.new("Kian", "Edalat")
+c2 = Customer.new("Sal", "Jackson")
+c3 = Customer.new("Ana", "Pak")
+c4 = Customer.new("Kian", "Peterson")
+
+r1 = Restaurant.new("Dishoom")
+r2 = Restaurant.new("Five Guys")
+r3 = Restaurant.new("Vapiano")
+r4 = Restaurant.new("Princi")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
