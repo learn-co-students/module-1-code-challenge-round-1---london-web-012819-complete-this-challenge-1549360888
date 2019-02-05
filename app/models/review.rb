4a7afe3c78265fb34d886a70e78238d3eae00768
Alexander Dubois <sander.dubois@outlook.com>
2019-02-05 10:52:31 +0000

    Deliverables delivered, checked all tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review
-  
-end
+  ##belongs to one resturant and one customer
 
+  attr_reader :customer, :resturant, :rating, :content
+
+  @@all = []
+
+  def initialize(resturant, customer, content, rating)
+    @resturant = resturant
+    @customer = customer
+    @content = content
+    @rating = rating
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,24 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+customer1 = Customer.new("Erik", "Erlandsson")
+customer2 = Customer.new("Erik", "Selin")
+customer3 = Customer.new("Jakob", "Dubois")
+customer4 = Customer.new("Agneta", "Johansson")
+
+resturant1 = Restaurant.new("Bella dolce")
+resturant2 = Restaurant.new("Novikov")
+resturant3 = Restaurant.new("Pret a manger")
+resturant4 = Restaurant.new("Tonys")
+
+review1 = Review.new(resturant1, customer1, "Extremly gooooooooddd, nice food", 4)
+review2 = Review.new(resturant2, customer3, "Okey", 1)
+review3 = Review.new(resturant1, customer4, "WWWWOOOWW", 5)
+review4 = Review.new(resturant4, customer1, "Meeh", 2)
+review5 = Review.new(resturant3, customer2, "Very nice location and people working there, will definently come ack here agian!", 4)
+review6 = Review.new(resturant1, customer3, "Desent", 3)
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
