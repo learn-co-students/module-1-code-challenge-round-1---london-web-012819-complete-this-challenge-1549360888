377456ed8bf6388f1fbc890e61da88c0f224eed7
saqemlas <saqemlas@googlemail.com>
2019-02-05 10:50:50 +0000

    progress

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,19 @@
 class Review
-  
-end
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name, full_name)
+    @name = name
+    @full_name = full_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+end
