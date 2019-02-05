95af98fc85d9e813753b7e601db97900a0016fb7
SofiaFredriksson <sofiapkf@gmail.com>
2019-02-05 10:37:15 +0000

    Customer done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,44 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|customer| name.include?(customer.first_name && customer.last_name)}
+  end
+
+  def self.find_all_by_first_name(name)
+    @@all.find_all {|customer| name == customer.first_name }
+  end
+
+  def self.all_names
+    @@all.map {|customer| customer.full_name }
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def num_reviews
+    Review.all.select {|review| review.customer == self}.length
+  end
+
+  def restaurants
+    arr = Review.all.select {|review| review.customer == self}
+    arr.map { |review| review.restaurant  }
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,31 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Customer.new("Sofia", "Fredriksson")
+c2 = Customer.new("Elisabet", "Silje")
+c3 = Customer.new("Karl", "Fredriksson")
+c4 = Customer.new("Ricky", "McCaw")
+c5 = Customer.new("Luc", "Batty")
+ce6 = Customer.new("Karl", "Silje")
+
+rest1 = Restaurant.new("Bone Daddies")
+rest2 = Restaurant.new("Mc Donalds")
+rest3 = Restaurant.new("Ceviche")
+rest4 = Restaurant.new("Pure")
+rest5 = Restaurant.new("Yo Sushi!")
+rest6 = Restaurant.new("Ceviche")
+
+rev1 = Review.new(c1, rest1, 5, "Tasty ramen! Highly recommend!")
+rev2 = Review.new(c2, rest3, 2, "Cheap and gross")
+rev3 = Review.new(c3, rest2, 4, "Good value!")
+rev4 = Review.new(c4, rest4, 3, "Ok for lunch")
+rev5 = Review.new(c5, rest5, 2, "Saw a mouse!")
+rev5 = Review.new(c5, rest1, 4, "Delicious!")
+rev5 = Review.new(c1, rest3, 2, "Good for groups")
+rev5 = Review.new(c5, rest3, 2, "Delicious food!")
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
